ca2c4a6222f24ff40c4dedeb0958d8bc5db1e41c
Hassaan Muzammil <52124348+hassaanmuzammil@users.noreply.github.com>
2023-02-25 17:59:45 +0500

    Update parse_japanese_address.py

--- parse_japanese_address.py
+++ parse_japanese_address.py
@@ -2,7 +2,7 @@ from japanese_addresses import separate_address
 import re
 
 def parse_japanese_address(address):
-
+    
     parsed = separate_address(address)
     
     pref = parsed.prefecture
@@ -31,6 +31,10 @@ def parse_japanese_address(address):
     if re.match(pattern_1, remaining): # '2丁目4-9'
         matched = re.findall(pattern_1, remaining)[0]
         chome, block = matched
+    elif re.match(pattern_7, remaining): # '1丁目9番地12号'
+        matched = re.findall(pattern_7, remaining)[0]
+        chome, block, building = matched
+        block = block + '-' + building
     elif re.match(pattern_2, remaining): # '23丁目4'
         matched = re.findall(pattern_2, remaining)[0]
         chome, block = matched
@@ -40,17 +44,31 @@ def parse_japanese_address(address):
     elif re.match(pattern_4, remaining): # '22-2-2'
         matched = re.findall(pattern_4, remaining)[0]
         chome, block = matched
+        chome = chome + '丁目'
     elif re.match(pattern_5, remaining): # '7643-12'
         matched = re.findall(pattern_5, remaining)[0]
         block = matched
     elif re.match(pattern_6, remaining): # '22'
         matched = re.findall(pattern_6, remaining)[0]
         block = matched
-    elif re.match(pattern_7, remaining): # ''1丁目9番地-12号''
-        matched = re.findall(pattern_7, remaining)[0]
-        chome, block, building = matched
-        block = block + '-' + building
     else:
         pass
     
     return pref, city, town, chome, block
+
+if __name__ == "__main__":
+    
+    address_test_list = [
+        '群馬県高崎市倉渕町川浦3900-156',
+        '群馬県高崎市倉渕町岩氷19-1',
+        '兵庫県神戸市東灘区住吉本町１丁目２番９',
+        '兵庫県神戸市東灘区岡本５丁目１',
+        '兵庫県神戸市灘区摩耶山町2-2',
+        '東京都港区麻布台1丁目9番地12号',
+        '東京都港区麻布台1-9-12',
+        '大阪府大阪市港区築港３丁目５-9',
+        '群馬県伊勢崎市連取町2335-1'
+    ]
+    
+    for address in address_test_list:
+        print(parse_japanese_address(address))
